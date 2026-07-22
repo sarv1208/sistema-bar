5620242d94e5dec1bf703ff76cad7c6737975dc1
sebastian <administracion@easydev-ve.com>
2026-07-22 18:56:51 -0400

    Force HTTPS scheme on Vercel production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (request()->header('x-forwarded-proto') == 'https' || app()->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         if (Schema::hasTable('settings')) {
             $settings = Setting::first();
 
