648acc103ba1f13a0becd3ce5685d7622c226931
sebastian <administracion@easydev-ve.com>
2026-07-22 21:36:25 -0400

    Fix duplicate Access-Control-Allow-Origin header

--- api/index.php
+++ api/index.php
@@ -1,10 +1,11 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Bar-Assistant-Bar-Id, Accept, Origin');
-
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    $origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+    header("Access-Control-Allow-Origin: {$origin}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Bar-Assistant-Bar-Id, Accept, Origin');
     http_response_code(200);
     exit(0);
 }
