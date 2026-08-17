b8d26c310e298b1680f29ecd5c920e98b6ce07c2
sebastian <administracion@easydev-ve.com>
2026-08-17 14:41:27 -0400

    Fix array_filter for PDO_MYSQL options

--- config/database.php
+++ config/database.php
@@ -58,10 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? [
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', null),
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
-            ] : [],
+            ], function ($value) { return !is_null($value); }) : [],
         ],
 
         'mariadb' => [
